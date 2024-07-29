e3e9d6d32429d0893365e37a0b348307d54cca29
tridiv007 <139316706+tridiv007@users.noreply.github.com>
2024-07-29 18:01:11 +0000

    feat: initiated the project

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,14 @@
 const hre = require("hardhat");
+const fs = require("fs");
 
 async function main() {
-  const contract = await hre.ethers.deployContract("Swisstronik", ["Hello Swisstronik from feature_earning!!"]);
+  const [deployer] = await hre.ethers.getSigners();
+  const contractFactory = await hre.ethers.getContractFactory("PrivateNFT");
+  const contract = await contractFactory.deploy(deployer.address);
   await contract.waitForDeployment();
-  console.log(`Swisstronik contract deployed to ${contract.target}`);
+  const deployedContract = await contract.getAddress();
+  fs.writeFileSync("contract.txt", deployedContract);
+  console.log(`Contract deployed to ${deployedContract}`);
 }
 
 main().catch((error) => {
